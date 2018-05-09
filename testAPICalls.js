3ee471aea7c06ef734983fba0d8a2fe909340344
anton <anton@mfin.co.za>
2018-05-09 07:39:10 +0200

    contact calls updated reset password flow updated generate2fa call added
    Signed-off-by: anton <anton@mfin.co.za>

--- testAPICalls.js
+++ testAPICalls.js
@@ -16,18 +16,27 @@ var sha256 = require('sha256');
 var token = null
 var user = null
 
-//callAPI('account/login', 'POST', { usernameOrEmail: 'test@test.com', password: '123456789' }, token, loginReturned)
+//callAPI('account/login', 'POST', { usernameOrEmail: 'anton@cryptocurve.io', password: '123123123' }, token, loginReturned)
 
-//callAPI('account/sendResetPasswordEmail', 'POST', { email: 'antonnell.crypto@gmail.com', callbackURL: 'http://localhost:3000' }, null, logData)
-callAPI('account/resetPassword', 'POST', { email: 'antonnell.crypto@gmail.com', password:'123123123', code:'CfDJ8N7FFOxtO0pAhVPgOFbi9L6VmRBEctt4jmtAy25Pm7W%2FdN4vlwVzaEj4xY0Ti8SYzfdHYjho8gbOvqR8dJbeMHOXdGhP9DMmgW2Rs54fzssCeXZULWfzR2xflim1kk5ZQ2EvMfPAIT%2F1qk0pt3PP3wLYOeQrf4UevA8qXq2SFYeQ8n3aA%2FSsQAe4WWVY%2BeZUllhDYoeE14kwEZQCY%2BA5QjB3KjYKSdsWdtNndfIz%2FW1FLjnuNc9kjFK0mCZTo%2B%2BmVQ%3D%3D' }, null, logData)
+//callAPI('account/sendResetPasswordEmail', 'POST', { email: 'anton@cryptocurve.io', callbackUrl: 'http://localhost:3000/#resetPassword' }, token, logData)
+/*callAPI('account/resetPassword',
+        'POST',
+        {
+           password:'123123123',
+           token: 'decodedURL',
+           code:'decodedURL'
+         },
+         null,
+         logData)//*/
+//callAPI('account/register', 'POST', { username: 'test1', email: 'test@test.com', password: '123123123' }, token, logData)
 
 function callAPIFunctions() {
-  //callAPI('account/register', 'POST', { username: 'test1', email: 'test1@test.com', password: '12345678' }, token, logData)
   //callAPI('account/updatePassword', 'POST', { username: user.username, password: '123456789' }, token, logData)
+  //callAPI('account/generate2faKey/'+user.id, 'GET', null, token, logData)
 
-  //callAPI('contacts/getUserContacts/'+user.username, 'GET', null, token, logData)
-  //callAPI('contacts/addUserContact', 'PUT', { username: 'test1', displayName: 'firend1', notes: 'note on friend1', ownerUsername: user.username }, token, logData)
-  //callAPI('contacts/updateUserContact', 'PUT', { username: 'test1', displayName: 'firend1', notes: 'note on friend1', ownerUsername: user.username }, token, logData)
+  //callAPI('contacts/getUserContacts/'+user.id, 'GET', null, token, logData)
+  //callAPI('contacts/addUserContact', 'POST', { emailOrUsername: 'anton@cryptocurve.io', displayName: 'firend1', notes: 'note on friend1', ownerUsername: user.username }, token, logData)
+  //callAPI('contacts/updateUserContact', 'POST', { username: 'anton@cryptocurve.io', displayName: 'My best friend', notes: 'Anton from CryptoCurve', ownerUsername: user.username }, token, logData)
 
   //callAPI('ethereum/getUserAddresses/'+user.id, 'GET', null, token, logData)
   //callAPI('ethereum/createAddress', 'POST', { name: 'My New Eth Address', isPrimary: true }, token, logData)
@@ -41,9 +50,9 @@ function callAPIFunctions() {
 
 function loginReturned(url, error, data) {
   if (error) {
-    console.log("---------------------------\n"+url+"\n---------------------------- \n"+JSON.stringify(error, null, 2))
+    console.log("---------------------------\n"+url+"\n----------------------------\nERROR\n"+error)
   } else {
-    console.log("----------------------------\n"+url+"\n---------------------------- \n"+JSON.stringify(data, null, 2))
+    console.log("----------------------------\n"+url+"\n----------------------------\nSUCCESS\n"+JSON.stringify(data, null, 2))
   }
 
   if (data.success) {
@@ -58,9 +67,9 @@ function loginReturned(url, error, data) {
 
 function logData(url, error, data) {
   if (error) {
-    console.log("\n\n----------------------------\n"+url+"\n---------------------------- \n"+JSON.stringify(error, null, 2))
+    console.log("\n\n----------------------------\n"+url+"\n----------------------------\nERROR\n"+error)
   } else {
-    console.log("\n\n----------------------------\n"+url+"\n---------------------------- \n"+JSON.stringify(data, null, 2))
+    console.log("\n\n----------------------------\n"+url+"\n----------------------------\nSUCCESS\n"+JSON.stringify(data, null, 2))
   }
 }
 
@@ -92,6 +101,17 @@ function callAPI(url, method, postData, token, callback){
       method: method,
       body: postData,
       headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer '+token },
+  })
+  .then(res => {
+    if(res.status == 401) {
+      throw Error('Unauthorised');
+    }
+
+    if (res.ok) {
+      return res;
+    } else {
+      throw Error(res.statusText);
+    }
   })
   .then(res => res.json())
   .then((res) => {
